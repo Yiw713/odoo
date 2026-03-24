cd2e99cecf3c56f33bf5c33222788271d1a85704
Yiw713 <wyutthaken@monogps.com>
2026-03-24 15:56:32 +0700

    1

--- addons/crm_custom/__manifest__.py
+++ addons/crm_custom/__manifest__.py
@@ -8,15 +8,7 @@
     'security/ir.model.access.csv',
     'data/crm_stage_data.xml',
     'data/currency_data.xml',
-    'views/crm_lead_view.xml',
-    'views/crm_followup_views.xml',
-    'views/crm_followup_dashboard_views.xml',
-    'views/crm_finance_dashboard_views.xml',
-    # 'views/crm_custom_menu.xml',
-    # 'views/crm_custom_action.xml',
-    # 'views/crm_custom_kanban.xml',
-    # 'views/crm_custom_form.xml',
-    
+    'views/crm_lead_view.xml',  
 ],
 'post_init_hook': 'update_crm_stages',
 
--- addons/crm_custom/models/__init__.py
+++ addons/crm_custom/models/__init__.py
@@ -1,5 +1,2 @@
 from . import crm_lead
 from . import custom_log
-from . import crm_followup
-from . import crm_followup_dashboard
-from . import crm_finance_dashboard
